29a41c507391575edfb7054871ecab54fce76978
hafiz <27100461@lums.edu.pk>
2026-03-16 17:05:59 +0500

    added a comment

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -4,6 +4,7 @@ public class City {
     private String city;
     private String province;
 
+//    this is the constructor
     public City(String city, String province){
         this.city = city;
         this.province = province;
